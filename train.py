e7eb74a839a8c03aa1234dd8d5affa539c9235e1
LonsonZheng <55726730+LonsonZheng@users.noreply.github.com>
2023-11-29 17:06:55 +0800

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,6 @@ import shutil
 import tqdm
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-
 import cv2
 import warnings
 warnings.filterwarnings('ignore')
@@ -21,6 +20,7 @@ import structure
 import netloss
 from load_data import *
 import time
+from utils import RawToSpike, cal_para
 
 def setup_seed(seed):
      torch.manual_seed(seed)
@@ -156,71 +156,6 @@ def evaluate(model, q, nd, nl, psnr, writer, iter, val_loader):
 	writer.add_scalar('PSNR', total_psnr.item(), iter)
 	torch.cuda.empty_cache()
 	return total_psnr, total_psnr_traditional, total_psnr_fusion, total_psnr_denoise, total_psnr_refine
-
-
-def RawToSpike(video_seq, h, w):
-	video_seq = np.array(video_seq).astype(np.uint8)
-	img_size = h*w
-	img_num = len(video_seq)//(img_size//8)
-	SpikeMatrix = np.zeros([img_num, h, w], np.uint8)
-	pix_id = np.arange(0, h*w)
-	pix_id = np.reshape(pix_id, (h, w))
-	comparator = np.left_shift(1, np.mod(pix_id, 8))
-	byte_id = pix_id // 8
-
-	for img_id in np.arange(img_num):
-		id_start = img_id*img_size//8
-		id_end = id_start + img_size//8
-		cur_info = video_seq[id_start:id_end]
-		data = cur_info[byte_id]
-		result = np.bitwise_and(data, comparator)
-		SpikeMatrix[img_id, :, :] = np.flipud((result == comparator))
-
-	return SpikeMatrix
-
-
-def cal_para():
-
-	lux1 = 0
-	lux2 = 153
-	lux3 = 335
-
-	# load and process light data
-	light_data = open("./uniformlight_new/light.dat", 'rb+')
-	light_seq = light_data.read()
-	light_seq = np.fromstring(light_seq, 'B')
-	light_spike = RawToSpike(light_seq, 250, 416)[:, :, :400]  # c*h*w
-	light_img = light_spike[0:20000].mean(axis=0).astype(np.float32)
-	# cv2.imwrite("./uniformlight/light.png", light_img * 255)
-	D_light = 1 / light_img - 1
-
-	# load meduim  data
-	meduim_data = open("./uniformlight_new/medium.dat", 'rb+')
-	meduim_seq = meduim_data.read()
-	meduim_seq = np.fromstring(meduim_seq, 'B')
-	meduim_spike = RawToSpike(meduim_seq, 250, 416)[:, :, :400]  # c*h*w
-	meduim_img = meduim_spike[0:20000].mean(axis=0).astype(np.float32)
-	# cv2.imwrite("./uniformlight/medium.png", meduim_img * 255)
-	D_meduim = 1 / meduim_img - 1
-
-	# load dark  data
-	dark_data = open("./uniformlight_new/dark.dat", 'rb+')
-	dark_seq = dark_data.read()
-	dark_seq = np.fromstring(dark_seq, 'B')
-	dark_spike = RawToSpike(dark_seq, 250, 416)[:, :, :400]  # c*h*w
-	dark_img = dark_spike[0:20000].mean(axis=0).astype(np.float32)
-	# cv2.imwrite("./uniformlight/dark.png", dark_img * 255)
-	D_dark = 1 / (dark_img + 1e-5) - 1
-
-	th = D_meduim / D_dark
-	fo1 = (lux2 * th - lux1) / (1 - th)
-
-	base = D_light[0, 0]
-	fpn_test = base * (lux3 + fo1[0, 0]) / (D_light * (lux3 + fo1))
-
-	q = D_light * (lux3 + fo1)
-
-	return q, fo1, fpn_test
 
 
 def main():
